bb2b2f6542ce40b99a04e7e7ac8cdadf6a2f68a8
Javier Shavy <55815625+JavierShavy@users.noreply.github.com>
2024-04-27 18:59:50 -0500

    Agregados archivos
    Agregados archivos de código py y cuaderno ipynb

---
+++ Clase03/0_Ejemplo_Python.py
@@ -0,0 +1,20 @@
+print (type("hola"))  #Muestra el tipo de dato escrito en type
+
+#La siguiente es una variable
+""""
+variable = 9  
+variable = 9  
+variable = 9  
+variable = 9  
+variable = 9  
+variable = 9  
+variable = 9  
+"""
+#Para comentar un bloque de código presionar Command + K, Command + C
+#Descomentar Command + K, Command + U
+#También con """" Tres comillas dobles
+variable_dos = "Mi variable" #Sintaxis recomendada en phyton para declarar variables con más palabras _
+#Preferiblemente declarar las variables en inglés
+print (variable_dos)
+my_string = "thisStringisAwesome"
+print (my_string.upper())
